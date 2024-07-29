9dc7bfa63d657d3bb121d05894c771a73cc72555
FirsovCA <169061803+FirsovCA@users.noreply.github.com>
2024-07-29 10:07:57 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-function changeText () {
-    const element = document.getElementById("text");
-    element.innerHTML = "Modified Text";
+let clickCount = 0;
+
+function countClick() {
+    clickCount++;
+    document.getElementById("clickCount")
+        .innerText = 'You have clicked ' + clickCount + ' times';
 }
-
-function changeColor () {
-    const element = document.getElementById("colorBox");
-
-    if (element.style.backgroundColor === "red") {
-        element.style.backgroundColor = 'green';
-    } else  element.style.backgroundColor = 'red';
-}
-
-function updateValue () {
-    const inputElement = document.getElementById("myInput");
-    inputElement.value = "Modified Value";
-}
-
-function removeElement () {
-    const element = document.getElementById("removeText");
-    if (element) element.parentNode.removeChild(element);
-}
-
